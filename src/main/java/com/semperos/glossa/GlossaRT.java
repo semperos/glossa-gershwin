7bd831422a9ae3236e4e14fa79d53d69a12d1fea
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-04-18 14:03:26 -0400

    Factor out beginnings of GlossaRT, make sure clojure.core is avaialble in glossa.core

---
+++ src/main/java/com/semperos/glossa/GlossaRT.java
@@ -0,0 +1,21 @@
+package com.semperos.glossa;
+
+import clojure.lang.IFn;
+import clojure.lang.Namespace;
+import clojure.lang.Symbol;
+
+public class GlossaRT {
+    final static private IFn IN_NS = ClojureApi.var("clojure.core", "in-ns");
+    final static private IFn IMPORT = ClojureApi.var("clojure.core", "import");
+    final static private IFn REFER = ClojureApi.var("clojure.core", "refer");
+    final static private Symbol STACK_CLASS_SYM = Symbol.intern("GlossaStack");
+    final static private Symbol CLOJURE = Symbol.intern("clojure.core");
+    final static private Symbol GLOSSA = Symbol.intern("glossa.core");
+    final static private Namespace GLOSSA_NS = Namespace.findOrCreate(GLOSSA);
+
+    static void doInit() {
+        IN_NS.invoke(GLOSSA);
+        GLOSSA_NS.importClass(STACK_CLASS_SYM, GlossaStack.class);
+        REFER.invoke(CLOJURE);
+    }
+}
--- src/main/java/com/semperos/glossa/main.java
+++ src/main/java/com/semperos/glossa/main.java
@@ -1,10 +1,7 @@
 package com.semperos.glossa;
 
-import clojure.lang.IFn;
 import clojure.lang.LineNumberingPushbackReader;
-import clojure.lang.Namespace;
 import clojure.lang.RT;
-import clojure.lang.Symbol;
 
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -13,11 +10,7 @@ import java.io.OutputStreamWriter;
  * Old-school debug REPL taken from commented-out Clojure main in LispReader
  */
 public class main {
-    final static private IFn IN_NS = ClojureApi.var("clojure.core", "in-ns");
-    final static private IFn IMPORT = ClojureApi.var("clojure.core", "import");
-    final static private Symbol STACK_CLASS_SYM = Symbol.intern("GlossaStack");
-    final static private Symbol GLOSSA_NS_SYM = Symbol.intern("glossa.core");
-    final static private Namespace GLOSSA_NS = Namespace.findOrCreate(GLOSSA_NS_SYM);
+
 
     public static void main(String[] args) {
 	LineNumberingPushbackReader r = new LineNumberingPushbackReader(new InputStreamReader(System.in));
@@ -25,8 +18,7 @@ public class main {
 	Object ret = null;
         // @todo Make sure clojure.core is available in glossa.core.
         // @todo Work on how GlossaStack operations themselve affect the stack (e.g., seq())
-        IN_NS.invoke(GLOSSA_NS_SYM);
-        GLOSSA_NS.importClass(STACK_CLASS_SYM, GlossaStack.class);
+        GlossaRT.doInit();
 	try {
             for(; ;) {
                 ret = GlossaCompiler.load(r);
