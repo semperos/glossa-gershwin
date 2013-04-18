124bd7432dda67f0a8b2f1ea519a0e6a8f12a5dc
Daniel Gregoire <daniel.l.gregoire@gmail.com>
2013-04-18 13:52:12 -0400

    Improve REPL

--- src/main/java/com/semperos/glossa/main.java
+++ src/main/java/com/semperos/glossa/main.java
@@ -1,7 +1,10 @@
 package com.semperos.glossa;
 
+import clojure.lang.IFn;
 import clojure.lang.LineNumberingPushbackReader;
+import clojure.lang.Namespace;
 import clojure.lang.RT;
+import clojure.lang.Symbol;
 
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
@@ -10,13 +13,23 @@ import java.io.OutputStreamWriter;
  * Old-school debug REPL taken from commented-out Clojure main in LispReader
  */
 public class main {
+    final static private IFn IN_NS = ClojureApi.var("clojure.core", "in-ns");
+    final static private IFn IMPORT = ClojureApi.var("clojure.core", "import");
+    final static private Symbol STACK_CLASS_SYM = Symbol.intern("GlossaStack");
+    final static private Symbol GLOSSA_NS_SYM = Symbol.intern("glossa.core");
+    final static private Namespace GLOSSA_NS = Namespace.findOrCreate(GLOSSA_NS_SYM);
+
     public static void main(String[] args) {
 	LineNumberingPushbackReader r = new LineNumberingPushbackReader(new InputStreamReader(System.in));
 	OutputStreamWriter w = new OutputStreamWriter(System.out);
 	Object ret = null;
+        // @todo Make sure clojure.core is available in glossa.core.
+        // @todo Work on how GlossaStack operations themselve affect the stack (e.g., seq())
+        IN_NS.invoke(GLOSSA_NS_SYM);
+        GLOSSA_NS.importClass(STACK_CLASS_SYM, GlossaStack.class);
 	try {
             for(; ;) {
-                ret = GlossaParser.read(r, true, null, false);
+                ret = GlossaCompiler.load(r);
                 RT.print(ret, w);
                 w.write('\n');
                 if(ret != null)
